54f1a0523678bbbfa59c1adc3c0ef00258739761
bbutz <bbutz@ransom-briggss-macbook-pro-15.local>
2011-01-26 22:21:56 -0600

    Nice little graphs

--- selector.rb
+++ selector.rb
@@ -4,13 +4,58 @@ class Selector
   end
 
   def select_best
+    potentials = []
+
+    # check the right
+    #     .] . .
+    #    .
+    #   .
+    # [.
     min = @array.min
     min_index = @array.index(min)
-    best_min = [min, @array[min_index..-1].max]
+    min_array = @array[min_index..-1]
+    min_array_max = min_array.max
+    min_array_max_index = min_array.index(min_array_max) + min_index
+    potentials << [difference([min, min_array_max]), [min_index, min_array_max_index]]
+
+    # check the left
+    #    .]
+    #   . |
+    #  .  |
+    #[.   | 
+    #     . .
     max = @array.max
     max_index = @array.index(max)
-    best_max = [@array[0..max_index].min, max]
-    difference(best_min) > difference(best_max) ? best_min : best_max
+    max_array = @array[0..max_index]
+    max_array_min = max_array.min
+    max_array_min_index = max_array.index(max_array_min)
+    potentials << [difference([max_array_min, max]), [max_array_min_index, max_index]]
+
+    # check the middleA
+    #   .   
+    # .  |    .] 
+    #    |  .  |
+    #    |     |
+    #    [.     |
+    #            .
+    if min_index < max_index
+      mid_array = @array[min_index+1..max_index-1]
+      offset = min_index + 1
+    else
+      mid_array = @array[max_index+1..min_index-1] if mid_array.empty?
+      offset = max_index + 1
+    end
+
+    unless mid_array.empty?
+      mid_array_min = mid_array.min
+      mid_array_max = mid_array.max
+      mid_array_min_index = mid_array.index(mid_array_min) + offset
+      mid_array_max_index = mid_array.index(mid_array_max) + offset
+      potentials << [difference([mid_array_min, mid_array_max]), [mid_array_min_index, mid_array_max_index]]
+    end
+    
+    # grab the potential with the highest difference and return the indexes
+    potentials.sort.reverse.first.last
   end
 
   private
--- spec/selector_spec.rb
+++ spec/selector_spec.rb
@@ -1,8 +1,8 @@
 require './selector'
 describe 'Selector' do
-  describe 'select_best' do
+  describe 'select_best_indices' do
     it 'should select min and max in an ordered set' do
-      Selector.new([1,2,3,4,5]).select_best.should == [1,5]
+      Selector.new([1,2,3,4,5]).select_best.should == [0,4]
     end
 
     it 'should select min and best max when max isnt in range' do
@@ -10,21 +10,20 @@ describe 'Selector' do
     end
 
     it 'should select best min and max when min isnt in range' do
-      Selector.new([2,3,4,5,1]).select_best.should == [2,5]
+      Selector.new([2,3,4,5,1]).select_best.should == [0,3]
     end
 
     it 'shouldnt freak out on duplicates' do
-      Selector.new([1,1,2,3,4,6,5,4,4]).select_best.should == [1,6]
+      Selector.new([1,1,2,3,4,6,5,4,4]).select_best.should == [0,5]
     end
 
     it 'should work for really obvious cases' do
-      Selector.new([1,6,2,3,4]).select_best.should == [1,6]
-      Selector.new([2,3,4,1,6]).select_best.should == [1,6]
+      Selector.new([1,6,2,3,4]).select_best.should == [0,1]
+      Selector.new([2,3,4,1,6]).select_best.should == [3,4]
     end
 
-    it 'should pick the first when theres a tie' do
-      Selector.new([3,5,2,4,1,3]).select_best.should == [3,5]
+    it 'should pick the middle when the largest range is in the middle' do
+      Selector.new([4,6,2,5,1]).select_best.should == [2,3]
     end
-
   end
 end
